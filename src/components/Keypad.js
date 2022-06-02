e2aacc664fb56e25dac50a9b1261f2136d6082f8
Cindy Germain <cindymarguerite@gmail.com>
2022-06-02 17:29:38 -0600

    commit tree 962dcb2d7fd53aaee182c2a04524f940cf1155ad

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+
+import React from 'react'
+
+function Keypad() {
+    function changeChallenge(){
+           console.log("Entering password...")
+
+    }
+
+  return (
+    
+        <input
+        type="password"
+        onChange={changeChallenge}
+           />
+  )
+}
+
+export default Keypad
